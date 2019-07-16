a9776a397774e8601a077193208c60f615089493
qucumbah <sc.buher@yandex.ru>
2019-07-16 08:51:33 +0300

    rewrite with express

--- index.js
+++ index.js
@@ -1,63 +1,43 @@
-const http = require("http");
-const url = require("url");
+const express = require("express");
 const dotenv = require("dotenv");
 const fetch = require("node-fetch");
-const mysql = require("mysql");
+const cors = require("cors")
 
 //requires .env file to have
 //API_KEY=yourYandexDictionaryApiKey
-//DB_HOST=dbHost
-//DB_USER=dbUser
-//DB_PASS=dbPass
-//DB_NAME=dbName
 dotenv.config();
 
-const con = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASS,
-	database: process.env.DB_NAME,
-});
+const app = express();
 
-http.createServer(async function(req, res) {
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader("Content-Type", "application/json");
+app.get('/translate', cors(), async function(req, res) {
+	const apiKey = process.env.API_KEY,
+				languageFrom = req.query.languageFrom,
+				languageTo = req.query.languageTo,
+				word = req.query.word;
 
-	if (req.url.startsWith("/translate")) {
-		const query = url.parse(req.url, true).query;
-		const apiKey = process.env.API_KEY,
-					languageFrom = query.languageFrom,
-					languageTo = query.languageTo,
-					word = query.word;
+	const request = await fetch(`https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=${apiKey}&lang=${languageFrom}-${languageTo}&text=`+word);
+	const obj = await request.json();
 
-		const request = await fetch(`https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=${apiKey}&lang=${languageFrom}-${languageTo}&text=`+word);
-		const obj = await request.json();
+	let code;
+	let errorMessage;
 
-		let code;
-		let errorMessage;
-
-    if (obj.code) { //Yandex API error
-      code = obj.code;
-			errorMessage = obj.message;
-    } else if (obj.def.length===0) { //No translations
-      code = 1;
-			errorMessage = "Unknown word";
-    } else { //All good
-      code = 200;
-    }
-
-		const result = {
-			code,
-			errorMessage,
-			def: obj.def
-		}
-
-		res.write(JSON.stringify(result));
-	} else if (req.url.startsWith("/login")) {
-
-	} else if (req.url.startsWith("/signup")) {
-
+	if (obj.code) { //Yandex API error
+		code = obj.code;
+		errorMessage = obj.message;
+	} else if (obj.def.length===0) { //No translations
+		code = 1;
+		errorMessage = "Unknown word";
+	} else { //All good
+		code = 200;
 	}
 
-	res.end();
-}).listen(5000);
+	const result = {
+		code,
+		errorMessage,
+		def: obj.def
+	}
+
+	res.send(JSON.stringify(result));
+});
+
+app.listen(5000);
