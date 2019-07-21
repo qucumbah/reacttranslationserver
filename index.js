a91d6aa7a22ff1071a97d74de51062f51c9ffa00
qucumbah <sc.buher@yandex.ru>
2019-07-21 10:55:44 +0300

    auth

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const session = require('express-session');
 const dotenv = require("dotenv");
 const fetch = require("node-fetch");
 const cors = require("cors")
+const Datastore = require('nedb');
+const NedbSessionStore = require('nedb-session-store')(session);
+const bcrypt = require('bcryptjs');
 
 //requires .env file to have
 //API_KEY=yourYandexDictionaryApiKey
-//SESS_SECRET=yourSecret
+//SESS_SECRET=yourSessionSecret
 dotenv.config();
 const {
 	API_KEY,
@@ -18,6 +21,14 @@ const {
 
 const app = express();
 
+const db = new Datastore({ filename: 'database/db.json' });
+db.loadDatabase(function(err) {
+	if (err) {
+		console.log("Error loading database:");
+		console.error(err);
+	}
+});
+
 app.use(express.static('public'));
 app.use(session({
 	name: SESS_NAME,
@@ -26,24 +37,124 @@ app.use(session({
 	secret: SESS_SECRET,
 	cookie: {
 		maxAge: SESS_LIFETIME,
-	}
+	},
+	store: new NedbSessionStore({
+		filename: 'database/sessions.json'
+	})
 }));
 
-app.post('/login', function(req, res) {
+app.use(express.urlencoded({ extended: true }));
 
+app.post('/login', function(req, res) {
+	const {login, password} = req.body;
+	db.find({ login: login.trim() }, async function(err, docs) {
+		if (err) {
+			res.json({ code: 1 });
+		} else if (docs.length===0) {
+			res.json({ code: 0 });
+		} else { //found user
+			const passwordCorrect = await bcrypt.compare(password, docs[0].hash);
+			if (passwordCorrect) {
+				req.session.login = login;
+				res.cookie('isLoggedIn', '1');
+				res.json({ code: 200 });
+			}
+		}
+	});
 });
 
-app.post('/signin', function(req, res) {
+app.post('/signup', async function(req, res) {
+	const {login, email, password1, password2} = req.body;
 
+	errorMessages = [];
+
+	if (!login.trim()) {
+		errorMessages.push('You have to enter your login');
+	}
+	if (!email.trim()) {
+		errorMessages.push('You have to enter your email');
+	}
+
+	if (!password1) {
+		errorMessages.push('You have to enter your password');
+	} else if (password1!==password2) {
+		errorMessages.push('Passwords dont match');
+	} else {
+		if (password1.length<9) {
+			errorMessages.push('Your password has to be at least 9 letters long');
+		}
+	}
+
+	if (errorMessages.length!==0) {
+		res.json({
+			code: 0,
+			errorMessages
+		});
+	} else {
+		const salt = await bcrypt.genSalt(10);
+		const hash = await bcrypt.hash(password1, salt);
+
+		//Check if user with this username/email already exists
+		db.find(
+			{ $or: [{ login: login.trim() }, { email: email.trim() }] },
+			function(err, docs) {
+
+				//Neighter username nor email have been used, everything ok
+				if (docs.length===0) {
+					db.insert({
+						login,
+						email,
+						hash
+					});
+					res.json({code: 200});
+				}
+
+				//Both username and email have been used
+				if (docs.length===2) {
+					res.json({
+						code: 0,
+						errorMessages: ['User with this login and email already exists']
+					});
+				}
+
+				//Eighter username or email has been used
+				if (docs.length===1) {
+					const usedLogin = docs[0].login===login.trim();
+					const usedEmail = docs[0].email===email.trim();
+
+					if (usedLogin && usedEmail) {
+						res.json({
+							code: 0,
+							errorMessages: ['User with this login and email already exists']
+						});
+					} else if (usedLogin) {
+						res.json({
+							code: 0,
+							errorMessages: ['User with this login already exists']
+						});
+					} else { //if (usedEmail)
+						res.json({
+							code: 0,
+							errorMessages: ['User with this email already exists']
+						});
+					}
+				}
+
+			}
+		);
+	}
 });
 
 app.post('/logout', function(req, res) {
-
+	req.session.destroy(err => {
+		res.clearCookie('isLoggedIn');
+		res.json({ code: 200 });
+	});
 });
 
 app.get('/userInfo', function(req, res) {
-	if (req.session.userLogin) {
-
+	if (req.session.login) {
+		res.json(req.session);
 	} else {
 		res.json({ code: 0 });
 	}
